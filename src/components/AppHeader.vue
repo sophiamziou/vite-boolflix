3037ec05d91d3bde047140118cbdebc82aee3100
sophiamziou <sofiamziou@gmail.com>
2023-01-17 23:52:04 +0100

    project created

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,7 @@
+<script>
+export default {};
+</script>
+<template lang="">
+  <div></div>
+</template>
+<style lang=""></style>
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    apiKey:"2f791f0062e16e067544f28c2d3e2018",
+    url: "https://api.themoviedb.org/3/search/movie?api_key=2f791f0062e16e067544f28c2d3e2018&language=en-US&query=",
+    moviesList:[],
+});
